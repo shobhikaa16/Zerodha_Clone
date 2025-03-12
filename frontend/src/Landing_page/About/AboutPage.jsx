af74691fbdf29bd09aac0744b4038f920fd76c29
Shobhikaa16 <shobhika.2226cs1170@kiet.edu>
2025-03-12 22:28:26 +0530

    fomt awesome

---
+++ frontend/src/Landing_page/About/AboutPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import About from './About'
+import Hero from './Hero'
+
+function AboutPage() {
+  return (
+    <>   
+       <Hero />
+       <About />
+    </>
+  )
+}
+
+export default AboutPage
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import HomePage from './Landing_page/Home/HomePage'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <div>
-    <h1>Hello!</h1>
-  </div>
+  
+    <HomePage></HomePage>
+ 
   </StrictMode>,
 )
