ea854e9216e6fec8b54501893be9b5cfefdb2663
Shobhikaa16 <shobhika.2226cs1170@kiet.edu>
2025-03-13 22:12:23 +0530

    HomePage Hero is set

--- frontend/src/Landing_page/Home/Hero.jsx
+++ frontend/src/Landing_page/Home/Hero.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 function Hero() {
   return (
-    <div> 
-      <FontAwesomeIcon icon={faEnvelope} />
+    <div className='container p-10'>
+      <div className='row text-center d-flex flex-column align-items-center'>
+        <img src='/images/homeHero.png' alt='Hero img' className='mb-5' style={{ width: "800px", height: "auto" }}/>
+        <h1 className='mt-5'>Invest in everything</h1>
+        <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+        <button  className= 'p-2 btn btn-primary fs-5' style={{width:"15%",margin: "0 auto"}}>Sign up for free</button>
+      </div>
+
     </div>
-  )
+        
+  );
 }
 
 export default Hero
