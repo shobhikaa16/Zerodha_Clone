792c13ad8ae4f59b18f5244b12f135940165ff75
Shobhikaa16 <shobhika.2226cs1170@kiet.edu>
2025-03-18 23:26:29 +0530

    awards ,hero

--- frontend/src/Landing_page/Home/Awards.jsx
+++ frontend/src/Landing_page/Home/Awards.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Awards() {
   return (
-    <div>Awards</div>
+    <div className='container mb-5'>
+      <div className='row'>
+        <div className='col-6'>
+           <img src="/images/largestBroker.svg" alt="" />
+        </div>
+        <div className='col-6'>
+           
+        </div>
+
+      </div>
+    </div>
   )
 }
 
