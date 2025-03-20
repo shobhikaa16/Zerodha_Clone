4bba35f8ab66d1e36857d70277c0be608e2ab11f
Shobhikaa16 <shobhika.2226cs1170@kiet.edu>
2025-03-20 23:45:19 +0530

    Pricing page

--- frontend/src/Landing_page/Home/Pricing.jsx
+++ frontend/src/Landing_page/Home/Pricing.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 function Pricing() {
   return (
-    <div>Pricing</div>
-  )
+    <div className="container p-5 ">
+      <div className="row">
+        <div className="col-5 ps-5"> 
+          <h1 className="mb-3">Unbeatable pricing</h1>
+          <p>
+            We pioneered the concept of discount broking and price transparency
+            in India. Flat fees and no hidden charges.{" "}
+          </p>
+          <a href="" className="ms-5" style={{ textDecoration: "none" }}>
+            Explore our products <FontAwesomeIcon icon={faArrowRight} />
+          </a>
+        </div>
+        <div className="col-1"></div>
+        <div className="col-6">
+        <div className="d-flex ">
+            <img src="images/z1.svg" alt="Image 1" className="img-fluid" style={{ maxWidth: "30%" }} />
+            <img src="images/z2.svg" alt="Image 2" className="img-fluid" style={{ maxWidth: "30%" }} />
+            <img src="images/z3.svg" alt="Image 3" className="img-fluid" style={{ maxWidth: "30%" }} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Pricing
+export default Pricing;
