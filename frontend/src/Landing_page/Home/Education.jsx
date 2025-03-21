58899543deaaa07a6a115de693e46215c7a7b3dc
Shobhikaa16 <shobhika.2226cs1170@kiet.edu>
2025-03-21 23:58:20 +0530

    some small edits

--- frontend/src/Landing_page/Home/Education.jsx
+++ frontend/src/Landing_page/Home/Education.jsx
@@ -15,13 +15,13 @@ function Education() {
                 the basics to advanced trading.
               </p>
               <a href="" className="" style={{ textDecoration: "none" }}>
-                Explore our products <FontAwesomeIcon icon={faArrowRight} />
+                Varsity <FontAwesomeIcon icon={faArrowRight} />
               </a>
               <p className='mt-5'>TradingQ&A, the most active trading and investment community in 
                 India for all your market related queries.
               </p>
               <a href="" className="" style={{ textDecoration: "none" }}>
-                Explore our products <FontAwesomeIcon icon={faArrowRight} />
+                TradingQ&A <FontAwesomeIcon icon={faArrowRight} />
               </a>
             </div>
           </div>
