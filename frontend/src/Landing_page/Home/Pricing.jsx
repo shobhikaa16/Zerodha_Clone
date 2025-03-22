728c5487722c6911b433efa1ac02e076ee94440c
Shobhikaa16 <shobhika.2226cs1170@kiet.edu>
2025-03-22 14:59:32 +0530

    navbar adjustment

--- frontend/src/Landing_page/Home/Pricing.jsx
+++ frontend/src/Landing_page/Home/Pricing.jsx
@@ -7,7 +7,7 @@ function Pricing() {
     <div className="container p-5 ">
       <div className="row">
         <div className="col-5 ps-5"> 
-          <h1 className="mb-3">Unbeatable pricing</h1>
+          <h1 className="mb-3 fs-2">Unbeatable pricing</h1>
           <p>
             We pioneered the concept of discount broking and price transparency
             in India. Flat fees and no hidden charges.{" "}
@@ -17,11 +17,15 @@ function Pricing() {
           </a>
         </div>
         <div className="col-1"></div>
-        <div className="col-6">
-        <div className="d-flex ">
+        <div className="col-6 mb-5">
+        <div className="d-flex gap-0 align-items-end">
             <img src="images/z1.svg" alt="Image 1" className="img-fluid" style={{ maxWidth: "30%" }} />
+            <p className="mt-5 text-xs me-5">Free account openeing</p>
             <img src="images/z2.svg" alt="Image 2" className="img-fluid" style={{ maxWidth: "30%" }} />
+            <p className="mt-5 text-xs me-5">Free account openeing</p>
             <img src="images/z3.svg" alt="Image 3" className="img-fluid" style={{ maxWidth: "30%" }} />
+            
+
           </div>
         </div>
       </div>
--- frontend/src/Landing_page/Navbar.jsx
+++ frontend/src/Landing_page/Navbar.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons"; // Import the icon
 
-function Navbar() {
+function MyNavbar() {
   return (
-    <div>Navbar</div>
-  )
+    <Navbar expand="lg" className="bg-white border-bottom">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            src="/images/logo.svg"
+            alt=""
+            className="p-3 ms-5"
+            style={{ width: "24%" }}
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto d-flex gap-4 fs-6  hover: text-primary">
+            <Nav.Link href="#link">Signup</Nav.Link>
+            <Nav.Link className='text-dark' href="#link">About</Nav.Link>
+            <Nav.Link className='text-dark' href="#link">Products</Nav.Link>
+            <Nav.Link className='text-dark' href="#link">Pricing</Nav.Link>
+            <Nav.Link className='text-dark' href="#link">Support</Nav.Link>
+            {/* FontAwesome icon for menu */}
+            <a href="#" className="text-dark mt-2 ms-1 ">
+              <FontAwesomeIcon icon={faBars} size="lg" />
+            </a>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Navbar
+export default MyNavbar;
