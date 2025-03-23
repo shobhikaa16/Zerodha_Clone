eab11713c32fc3f86d2d621ff8b4fe05a4dd74e0
Shobhikaa16 <shobhika.2226cs1170@kiet.edu>
2025-03-23 19:55:25 +0530

    LeftSection with props

--- frontend/src/Landing_page/Products/ProductPage.jsx
+++ frontend/src/Landing_page/Products/ProductPage.jsx
@@ -1,18 +1,46 @@
-import React from 'react'
-import LeftSection from './LeftSection'
-import RightSection from './RightSection'
-import Hero from './Hero'           
-import Universe from './Universe'
+import React from "react";
+import LeftSection from "./LeftSection";
+import RightSection from "./RightSection";
+import Hero from "./Hero";
+import Universe from "./Universe";
 
 function ProductPage() {
   return (
-    <>  
-       <Hero />
-       <LeftSection />
-       <RightSection />
-       <Universe />
+    <>
+      <Hero />
+      <LeftSection
+        imageURL="/images/kite.png"
+        productName="Kite"
+        productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appstore=""
+      />
+      <RightSection />
+      <LeftSection
+        imageURL="/images/coin.png"
+        productName="Coin"
+        productDescription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appstore=""
+      />
+      <RightSection />
+      <LeftSection
+        imageURL="/images/varsity.png"
+        productName="Varsity mobile"
+        productDescription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appstore=""
+      />
+      
+      <Universe />
     </>
-  )
+  );
 }
 
-export default ProductPage
+export default ProductPage;
