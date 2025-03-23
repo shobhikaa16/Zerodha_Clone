c13d4200e79034b9f252ec82677c0f9748b52902
Shobhikaa16 <shobhika.2226cs1170@kiet.edu>
2025-03-23 15:30:32 +0530

    About is ready

--- frontend/src/Landing_page/About/Hero.jsx
+++ frontend/src/Landing_page/About/Hero.jsx
@@ -3,14 +3,15 @@ import React from "react";
 function Hero() {
   return (
     <div className="container mt-5">
-      <div className="row mt-5 p-5 mb-5">
+      <div className="row mt-5 mb-5">
         <h1 className=" text-center ps-2" style={{ fontSize: "33px" }}>
           We pioneered the discount broking model in India.
           <br /> Now, we are breaking ground with our technology.
         </h1>
       </div>
-      <div className="row p-5 border-top">
-        <div className="col-6 text-muted ">
+
+      <div className="row p-5 mt-5 border-top text-muted " style={{lineHeight: "1.9",fontSize:"1em"}}>
+        <div className="col-6  p-5">
           <p>
             We kick-started operations on the 15th of August, 2010 with the goal
             of breaking all barriers that traders and investors face in India in
@@ -28,13 +29,13 @@ function Hero() {
             all Indian retail trading volumes.
           </p>
         </div>
-        <div className="col-6 text-muted">
+        <div className="col-6 p-5">
           <p>
             In addition, we run a number of popular open online educational and
             community initiatives to empower retail traders and investors.
           </p>
           <p>
-            <a href="">Rainmatter</a>, our fintech fund and incubator, has invested in several
+            <a href="" style={{textDecoration:"none"}}>Rainmatter</a>, our fintech fund and incubator, has invested in several
             fintech startups with the goal of growing the Indian capital
             markets.
           </p>
