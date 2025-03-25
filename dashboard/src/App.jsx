abfb975df8ad198c57178738c3015815cfeeb8f9
Shobhikaa16 <shobhika.2226cs1170@kiet.edu>
2025-03-25 09:58:42 +0530

    Setting up of dashboard

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <p className='fs-3'>Dashboard</p>
+    </div>
+  )
+}
+
+export default App
