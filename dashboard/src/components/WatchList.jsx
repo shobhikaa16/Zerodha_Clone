ace77380515568c6dda2810ed5e83a256aa09b91
Shobhikaa16 <shobhika.2226cs1170@kiet.edu>
2025-04-11 01:07:02 +0530

    Watchlist  started

--- dashboard/src/components/WatchList.jsx
+++ dashboard/src/components/WatchList.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function WatchList() {
+import { Tooltip, Grow } from "@mui/material";
+import { watchlist } from "../data/data";
+
+const WatchList = () => {
   return (
-    <div>WatchList</div>
-  )
-}
+    <div className="watchlist-container">
+      <div className="search-container">
+        <input type="text"
+        name="search"
+        id="search"
+        placeholder="Search eg: infy, bse, nifty fut weekly, gold mcx"
+        className="search" 
+        />
+        <span className="counts"> {watchlist.length} / 50</span>
+      </div>
+      <ul className="list">
+         {watchlist.map((stock,index)=>{
+            return (
+            <WatchListItem stock = {stock} key={index} />
+         )})}
+      </ul>
+    </div>
+  );
+};
 
-export default WatchList
+export default WatchList;
--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route } from "react-router-dom";
-import Apps from "./components/Apps"; // Ensure App.jsx exists
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Apps from "./components/Apps"; // This will act as the Home component
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -9,7 +9,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path ="/*" element={<Home/>} />
+        <Route path="/*" element={<Apps />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
