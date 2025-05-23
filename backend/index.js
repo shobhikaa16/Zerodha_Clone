1b653c263cbe76391229cd947ffcc4ccfbc0d32a
Shobhikaa16 <shobhika.2226cs1170@kiet.edu>
2025-05-23 18:58:04 +0530

    mongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3002; // agar yeh line miss out hojati hai oth deployment  ke time issue ata hai 
+const url = process.env.MONGO_URL;
 
 const app = express();
+
+
 app.listen(3002, ()=>{
     console.log("App started!")
-})
+    mongoose.connect(url)
+    console.log("DB connected!") // eh dono likh kar aarhe hai mtlb databse successfully connect hogya hai
+});
