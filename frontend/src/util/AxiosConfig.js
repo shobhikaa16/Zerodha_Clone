e23afc333c1cecae9f115bd4b64655bb1e265e7a
Shobhikaa16 <shobhika.2226cs1170@kiet.edu>
2025-07-14 19:47:32 +0530

    chnge backend server

--- frontend/src/util/AxiosConfig.js
+++ frontend/src/util/AxiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3002',
+  baseURL: 'https://zerodha-backend-c8sc.onrender.com/',
   withCredentials: true, // important for cookie-based auth
 });
 
